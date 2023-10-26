0171340afdf93fc9e2719148e3e45784d4facd74
Alex Granovsky <compfaculty@users.noreply.github.com>
2023-10-26 08:44:00 +0300

    refactoring

--- networking/get_url_basic.nim
+++
@@ -1,7 +0,0 @@
-import asyncdispatch, httpclient
-
-proc asyncProc(): Future[string] {.async.} =
-  var client = newAsyncHttpClient()
-  result = await client.getContent("http://example.com")
-
-echo waitFor asyncProc()
